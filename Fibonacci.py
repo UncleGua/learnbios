88a8c38625a58927e085063e15d3c1004001f585
sweetpotatozombie <62167563+sweetpotatozombie@users.noreply.github.com>
2020-03-16 22:48:56 +0800

    Update Fibonacci.py

--- Fibonacci.py
+++ Fibonacci.py
@@ -8,14 +8,4 @@ def fu(y,t):
         print (b)
      
       
-fu(30,2)#数列
-def fu(y,t):
-    f=[1,1]
-    a=f[1]
-    b=f[1]
-    for i in range(y-2):
-        a,b=b,b+t*a
-        print (b)
-     
-      
 fu(30,2)
